de0bf40b992477b3410c9a6383fc6c4b24d49fa6
kan.k <kan.k@siriustech.io>
2022-12-21 23:08:56 +0700

    Finish Explore Section

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -10,6 +10,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
 		className={`relative ${
 			active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
 		} flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+		onClick={() => handleClick(id)}
 	>
 		<img
 			src={imgUrl}
@@ -21,7 +22,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
 				{title}
 			</h3>
 		) : (
-			<div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
+			<div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
 				<div
 					className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
 				>
@@ -34,6 +35,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
 				<p className="font-normal text-[16px] leading-[20px] text-white uppercase  ">
 					Enter the Metaverse
 				</p>
+				<h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+					{title}
+				</h2>
 			</div>
 		)}
 	</motion.div>
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -7,7 +7,7 @@ import { staggerContainer } from '../utils/motion'
 import { exploreWorlds } from '../constants'
 
 const Explore = () => {
-	const [active, setActive] = useState(true)
+	const [active, setActive] = useState('world-2')
 
 	return (
 		<section className={`${styles.paddings}`} id="explore">
